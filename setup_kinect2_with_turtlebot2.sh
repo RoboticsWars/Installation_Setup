1732e835d6b73bd473c1409a29c9811e73268b39
WLwind <1121391435@qq.com>
2019-09-07 11:02:12 +0800

    修改复制kinect2_bridge的内容

--- setup_kinect2_with_turtlebot2.sh
+++ setup_kinect2_with_turtlebot2.sh
@@ -12,6 +12,6 @@ fi
 echo -e "\033[42;37mCopying files to related directories...\033[0m"
 sudo cp -r ~/Turtlebot2_Kinect2/TB2+Kinect2/turtlebot_description/. /opt/ros/kinetic/share/turtlebot_description/
 sudo cp -r ~/Turtlebot2_Kinect2/TB2+Kinect2/turtlebot_navigation/launch/. /opt/ros/kinetic/share/turtlebot_navigation/launch/
-cp -r ~/Turtlebot2_Kinect2/TB2+Kinect2/iai_kinect2/kinect2_bridge/launch/kinect2_laser.launch ~/catkin_ws/src/iai_kinect2/kinect2_bridge/launch/
+cp ~/Turtlebot2_Kinect2/TB2+Kinect2/iai_kinect2/kinect2_bridge/launch/* ~/catkin_ws/src/iai_kinect2/kinect2_bridge/launch/
 echo "export TURTLEBOT_3D_SENSOR=kinect2" >> ~/.bashrc && source ~/.bashrc
 echo -e "\033[42;37mSetup complete.\033[0m"
